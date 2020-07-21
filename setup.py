987c8b87fb40db3847738b26764aa6c9102efe9e
ChongChong He <che1234@umd.edu>
2020-07-20 22:27:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acap',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/chongchonghe/acap.git',
     author='Chong-Chong He',
     author_email='che1234@umd.edu',
