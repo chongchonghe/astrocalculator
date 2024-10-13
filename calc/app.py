c90159dbc43fa2091c732ddf5d3d483933d9b7c9
ChongChong He <chongchong.he@anu.edu.au>
2024-10-13 18:41:20 +1100

    automatically open browser

--- calc/app.py
+++ calc/app.py
@@ -1,3 +1,5 @@
+import webbrowser
+import threading
 from flask import Flask, render_template, request
 from calc import execute_calculation
 
@@ -23,5 +25,9 @@ def index():
                            result_si=result_si, result_cgs=result_cgs, 
                            result_user_units=result_user_units, user_units=user_units)
 
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    threading.Timer(1.25, open_browser).start()
+    app.run(debug=True, use_reloader=False)
